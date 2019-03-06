a541248731601ea1fc515f1890e3cc1f132d3d48
David Storey <dstorey65@gmail.com>
2019-03-05 19:03:52 -0600

    Children and parent playing nicely together, just need to format output.

--- child.c
+++ child.c
@@ -3,11 +3,13 @@
 #include<sys/ipc.h>
 #include<sys/shm.h>
 #include<sys/types.h>
-#include<string.h>
+#include<unistd.h>
+#include<stdlib.h>
 
 
 int main(int argc, char * argv[]){
 	char * ptr;
+	pid_t pid = getpid();
 	unsigned long * shmPTR;
 	key_t key = strtoul(argv[0], &ptr, 10);
 	unsigned long life = strtoul(argv[2], &ptr, 10);
@@ -15,11 +17,9 @@ int main(int argc, char * argv[]){
 	unsigned long existence;
 	shmID = shmget(key, sizeof(unsigned long), O_RDONLY);
 	shmPTR = (unsigned long *) shmat(shmID, NULL, 0);
-	printf("Child entered at %li with life %li.\n", shmPTR[0], life);
 	existence = shmPTR[0] + life;
-	printf("Exists until %li.\n", existence);
 	while(shmPTR[0] < existence);
-	printf("Child exiting at %li.\n", shmPTR[0]);
+	printf("Process %d exiting at %li\n", pid, shmPTR[0]);
 	shmdt(shmPTR);
 	return 0;
 }
--- scheduler1.c
+++ scheduler1.c
@@ -10,23 +10,31 @@
 
 
 void scheduler(char* input, char* outfile, int limit, int total){
-	int increment = 0, i = 0, k, status, alive = 1, noChildFlag = 1;
+	int increment = 0, i = 0, k, status, alive = 1, noChildFlag = 1, totalFlag = 0, timeFlag = 0, limitFlag = 0;
 	unsigned long nanoSec, life, shmID, seconds = 10000;
 	unsigned long * shmPTR;
 	char * parameter[32], parameter1[32], parameter2[32], parameter3[32];
 	pid_t pid[total], endID = 1; 
 	time_t when, when2;
 	FILE * fp;
+	FILE * outPut;
 	key_t key;
 	srand(time(0));
 	key = rand();
 	time(&when);
+	outPut = fopen(outfile, "a");
 	fp = fopen(input, "r");
 	//Check for file error.
+	if (outPut == NULL){
+		perror("Error");
+		printf("Output file could not be created.\n");
+		exit(EXIT_SUCCESS);
+	}
 	if (fp == NULL){
 		perror("Error");
 		printf("File given: %s, error number: %d\n", input, errno);
 		printf("No output generated.\n");
+		exit(EXIT_SUCCESS);
 	}
 	fscanf(fp, "%d", &increment);
 	shmID = shmget(key, sizeof(unsigned long), IPC_CREAT | IPC_EXCL | 0777);
@@ -35,41 +43,39 @@ void scheduler(char* input, char* outfile, int limit, int total){
 	for(k = 0; k < total; k++){
 		pid[k] = -1;
 	}
-//	while(alive > 0){
-	for(int j = 0; j < 3; j++){
+	while(alive > 0){
 		time(&when2);
 		if ((when2 - when) >= 2){
 			printf("Program has exceeded its allotted time, exiting.\n");
-			shmdt(shmPTR);
-			wait(NULL);
-			exit(EXIT_SUCCESS);
-		}
-		if (seconds > 1000){
-			if (fscanf(fp, "%d", &seconds) !=EOF){
-				fscanf(fp, "%li", &nanoSec);
-				fscanf(fp, "%li", &life);
-			}
+			timeFlag = 1;
 		}
 		shmPTR[0] += increment;
-		if(shmPTR[0] >= ((seconds * 1000000000) + nanoSec)){
-			if((pid[i] = fork()) == 0){
-				sprintf(parameter1, "%li", key);
-				sprintf(parameter2, "%li", shmID);
-				sprintf(parameter3, "%li", life);
-				char * args[] = {parameter1, parameter2, parameter3, NULL};
-				execvp("./child\0", args);
-			}
-			else{
-				printf("Child %d spawned.\n", pid[i]);
-				if (noChildFlag > 0){
-					alive--;
-					noChildFlag = 0;
+		if((timeFlag == 0) && (totalFlag == 0) && (limitFlag == 0)){
+			if (seconds > 1000){
+				if (fscanf(fp, "%d", &seconds) !=EOF){
+					fscanf(fp, "%li", &nanoSec);
+					fscanf(fp, "%li", &life);
 				}
-				seconds = 100000;
-				nanoSec = 0;
-				life = 0;
-				alive++;
-				i++;
+			}
+			if(shmPTR[0] >= ((seconds * 1000000000) + nanoSec)){
+				if((pid[i] = fork()) == 0){
+					sprintf(parameter1, "%li", key);
+					sprintf(parameter2, "%li", shmID);
+					sprintf(parameter3, "%li", life);
+					char * args[] = {parameter1, parameter2, parameter3, NULL};
+					execvp("./child\0", args);
+				}
+				else{
+					if (noChildFlag > 0){
+						alive--;
+						noChildFlag = 0;
+					}
+					seconds = 100000;
+					nanoSec = 0;
+					life = 0;
+					alive++;
+					i++;
+				}
 			}
 		}
 		endID = waitpid(0, &status, WNOHANG | WUNTRACED);
@@ -81,12 +87,14 @@ void scheduler(char* input, char* outfile, int limit, int total){
 			for(k = 0; k <= i; k++){
 				if(endID == pid[k]){
 					alive--;
-					if (alive > 0){
+					if (alive >= 0){
 						if (WIFEXITED(status)){
-							printf("Child ended normally.\n");
+							limitFlag = 0;
+							
 						}
 						else if (WIFSIGNALED(status)){
 							printf("Child ended with an uncaught signal, %d.\n", status);
+							limitFlag = 0;
 						}
 						else if (WIFSTOPPED(status)){
 							printf("Child process has stopped.\n");
@@ -97,11 +105,11 @@ void scheduler(char* input, char* outfile, int limit, int total){
 		}
 		if (alive > total){
 			printf("Program has exceeded its allotted children, exiting.\n");
-			shmdt(shmPTR);
-			wait(NULL);
-			exit(EXIT_SUCCESS);
+			totalFlag = 1;
+		}
+		if (alive > limit){
+			limitFlag = 1;
 		}
 	}
-	wait(NULL);
 	shmdt(shmPTR);
 }
